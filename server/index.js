fb44e8c4b21b51407604cfdec0215f4f8b7ff0a2
Saket <saketmodi787@gmail.com>
2026-08-25 02:31:36 +0530

    lmao no

--- server/index.js
+++ server/index.js
@@ -1,18 +1,9 @@
 // ref: https://bytebytego.com/courses/system-design-interview/design-a-url-shortener
 const express = require('express');
 const app = express.app();
-const { connectDB, lookUp } = require('./database');
+const { connectDB, lookUp, createEntry } = require('./database');
 
-app.post('/api/url/create', (req, res) => {
-    /*
-        {
-            "url": "sharan-deepak.india",
-            "short": "sd.india",
-            "last-updated": "now"
-        }
-    */
-    const to_encode = req.url;
-    // check if it exists as a key in the db
-    // if yes -> get -> push to client
-    // if no -> push to db -> push to client
+app.post('/api/create/:uri', (req, res) => {
+    const urlEntry = await createEntry(req.params.uri);
+    return urlEntry;
 });
